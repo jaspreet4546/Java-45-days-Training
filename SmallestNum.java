37a1df02da808c1d4f71088d5bfaa4debbaf1dc4
jaspreet4546 <135807447+jaspreet4546@users.noreply.github.com>
2023-06-15 18:40:49 +0530

    Add files via upload

---
+++ SmallestNum.java
@@ -0,0 +1,21 @@
+
+public class SmallestNum{
+    public static void main(String []args){
+int FirstNum = 25;
+int SecondNum = 37;
+int ThirdNum = 29;
+if(FirstNum < SecondNum){
+  if(FirstNum < ThirdNum){
+    System.out.println("First Number is the shortest number:"+FirstNum);
+}
+  else{
+    System.out.println(" Third number is smallest" +ThirdNum);
+}
+}
+else{
+ System.out.println("Second number is the shortest number" +SecondNum);
+}
+}
+}
+
+   
