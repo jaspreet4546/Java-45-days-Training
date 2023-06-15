468c914514370d4a342e84e8d8f28d3daec07839
jaspreet4546 <135807447+jaspreet4546@users.noreply.github.com>
2023-06-15 18:05:18 +0530

    Add files via upload

---
+++ FloatSum.java
@@ -0,0 +1,17 @@
+
+public class FloatSum{
+    public static void main(String []args){
+      float a = 94.64f;
+      float b = 85.35f;
+      float c = 54.36f;
+     float sum = a+b+c;
+System.out.println("The sum of 3 flaot values is :"+sum);
+    int d = (int)sum;
+System.out.println("The value in integer is:"+d);
+ 
+double e = sum;
+System.out.println("The value in double is :"+e);
+      
+
+}
+}
