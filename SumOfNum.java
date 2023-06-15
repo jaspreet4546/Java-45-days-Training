5f2c311be4a2644f19f536e73e254edfbcabfa3d
jaspreet4546 <135807447+jaspreet4546@users.noreply.github.com>
2023-06-15 18:51:39 +0530

    Add files via upload

---
+++ SumOfNum.java
@@ -0,0 +1,17 @@
+
+public class SumOfNum{
+   public int Sum(int a,int b){
+
+ int sum = a+b;
+  System.out.println("The sum of two numbers is:"+sum);
+
+ return sum;
+}
+    public static void main(String []args){
+    SumOfNum obj = new SumOfNum();
+    obj.Sum(9,6);
+
+}
+}
+
+   
