5e348bbd01cf4d6173b6b474984882dbce41f0d2
jaspreet4546 <135807447+jaspreet4546@users.noreply.github.com>
2023-06-15 19:19:33 +0530

    Add files via upload

---
+++ naturalNumbers.java
@@ -0,0 +1,11 @@
+public class naturalNumbers{
+    public static void main(String[]args){
+        int n=0;
+    for(int i=1;i<=10;i++){
+        n=n+i;
+       
+    }
+    System.out.println("Sum of first 10 natural numbers is:"+n);
+    }
+
+   
