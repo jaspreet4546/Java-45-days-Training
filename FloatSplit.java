e9dce8d545d04dc1162e25384a369d956839929d
jaspreet4546 <135807447+jaspreet4546@users.noreply.github.com>
2023-06-15 17:58:22 +0530

    Add files via upload

---
+++ FloatSplit.java
@@ -0,0 +1,13 @@
+
+public class FloatSplit{
+    public static void main(String []args){
+      float a = 94.64f;
+      System.out.println("The value of Float is:"+a);
+    int b=(int)a;
+       System.out.println("The value of float in integer is :"+b);
+    double c= a-b;
+       System.out.println("The value in double is:"+c);
+      
+
+}
+}
